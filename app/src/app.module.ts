0796f735457f9b364cd5f088911aa399c96b558e
Andrei Olar <andrei.olar@amdaris.com>
2024-02-08 16:11:38 +0200

    added back ping endpoint

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { CatShelvesModule } from './challange1/challange1.module';
 import { RestaurantOrderModule } from './challange2/challange2.module';
 import { SupermarketQueueModule } from './challange3/challange3.module';
+import { PingModule } from './ping/ping.module';
 
 @Module({
-  imports: [CatShelvesModule, RestaurantOrderModule, SupermarketQueueModule],
+  imports: [
+    CatShelvesModule,
+    RestaurantOrderModule,
+    SupermarketQueueModule,
+    PingModule,
+  ],
 })
 export class AppModule {}
